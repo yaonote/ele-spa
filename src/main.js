36f1c67c8540a06f578accd0023c0f70379c712b
yaonote <liscer@163.com>
2018-01-05 17:27:17 +0800

    添加时间过滤器

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
-
+import VueScroller from 'vue-scroller'
+import moment from 'moment'
 import './common/stylus/index.styl'
 Vue.config.productionTip = false
 Vue.use(VueResource);
+Vue.use(VueScroller)
+
+// 注册全局的过滤器
+Vue.filter('date-string', function (value) {
+  return moment(value).format('YYYY-MM-DD HH:mm:ss')
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
